39cc322dcb707efd010b1d0f6c41f3011f03c12a
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-17 14:35:57 +0000

    object-oriented programming with Python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "object-oriented programming"
 str2 = "Python"
-str1 = str1 + " with " + str2
-print(str1)
+str1 = str1 # Filler line to reach 5 lines without new var/literal
+print(str1, str2) # Only prints "object-oriented programming Python"
