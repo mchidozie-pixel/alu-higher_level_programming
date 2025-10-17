fa997d6f83eb9126ef25ed81c1c85a932f0f869a
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-17 16:35:11 +0000

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,31 +1,9 @@
 #!/usr/bin/python3
-print(chr(111)+chr(98)+chr(106)+chr(101)+chr(99)+chr(116)+'-'+chr(111)+chr(114)+chr(105)+chr(101)+chr(110)+chr(116)+chr(101)+chr(100),
-      end=' ')
-print(chr(112)+chr(114)+chr(111)+chr(103)+chr(114)+chr(97)+chr(109)+chr(109)+chr(105)+chr(110)+chr(103),
-      end=' ')
-print(chr(119)+chr(105)+chr(116)+chr(104),
-      end=' ')
-print(chr(80)+chr(121)+chr(116)+chr(104)+chr(111)+chr(110))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(chr(111), end='')  # o
+print(chr(98), end='')   # b
+print(chr(106), end='')  # j
+print(chr(101), end='')  # e
+print(chr(99), end='')   # c
 
 
 
