b9056fc664efbe57f86acee64af352f8e28e7a26
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-17 16:37:59 +0000

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,13 +3,3 @@ str1 = "object-oriented programming"
 str2 = "Python"
 str1 = str1 # Filler line to reach 5 lines
 print(f"{str1} with {str2}") # Fails: uses string literal " with "
-
-
-
-
-
-
-
-
-
-
