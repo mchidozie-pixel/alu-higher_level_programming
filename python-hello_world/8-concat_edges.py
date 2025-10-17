8a86dfea1103928be7c275931f3ce695a369f7ce
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-17 06:41:38 -0700

    object-oriented programming with Python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str1 = "object-oriented programming"
-str2 = " with Python"
-# Print by concatenating slices of existing variables only
-print(str1 + str2)
+str2 = "Python"
+# Missing line: MUST NOT contain new variables or string literals, and must print the result.
