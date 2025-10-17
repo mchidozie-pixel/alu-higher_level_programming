de6251edf79e802cbd59b565f3b74d56064dc4d7
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-17 16:16:53 +0000

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
-#!/usr/binpython3
-from add_0 
-if __nae__ == "__main__":
+#!/usr/bin/python3
+from add_0 import add
+
+
+if __name__ == "__main__":
     a = 1
-    b = 
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
