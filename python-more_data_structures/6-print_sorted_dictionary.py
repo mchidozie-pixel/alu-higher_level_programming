90b9993197ef2bd02f7947ce5e639b0fa4b350bb
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-29 12:07:32 +0000

    Implement print_sorted_dictionary function to print dictionary by alphabetically sorted keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    """
+    Prints a dictionary by ordered keys (alphabetical order).
+
+    Args:
+        a_dictionary (dict): The dictionary to print.
+    """
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
