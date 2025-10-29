8c425fb83343de07f254b41e0641ee80c8e48fb7
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-29 12:00:19 +0000

    Implement common_elements function to return the intersection of two sets

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of elements common to both set_1 and set_2.
+
+    Args:
+        set_1 (set): First set of elements.
+        set_2 (set): Second set of elements.
+
+    Returns:
+        set: Set containing elements found in both sets.
+    """
+    return set_1 & set_2
