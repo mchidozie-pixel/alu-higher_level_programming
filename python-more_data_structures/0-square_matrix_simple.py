1c7245332016bd255b70fe5a740dc587a18b31d2
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-29 11:52:40 +0000

    Implement square_matrix_simple function to return a new matrix with squared values

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)  # [[1, 4, 9], [16, 25, 36], [49, 64, 81]]
+print(matrix)      # Original matrix remains unchanged
