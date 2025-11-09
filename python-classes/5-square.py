dde96f94ace012ab0ff7014e0e5f5399b9c940a2
mchidozie-pixel <m.chidozie@alustudent.com>
2025-11-09 08:55:19 +0000

    Add my_print method to Square class to print the square using #

---
+++ python-classes/5-square.py
@@ -0,0 +1,22 @@
+my_square = Square(3)
+my_square.my_print()
+# ###
+# ###
+# ###
+
+my_square.size = 10
+my_square.my_print()
+# ##########
+# ##########
+# ##########
+# ##########
+# ##########
+# ##########
+# ##########
+# ##########
+# ##########
+# ##########
+
+my_square.size = 0
+my_square.my_print()
+# (prints an empty line)
