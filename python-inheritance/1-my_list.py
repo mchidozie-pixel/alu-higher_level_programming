fb24461b4a5fbf76ecfc38607e2b01fe8cd960f6
mchidozie-pixel <m.chidozie@alustudent.com>
2025-11-14 14:12:45 +0000

    work

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Module 1-my_list.
-Creates a class inheriting from the list class.
-"""
+''' Module: 1-my_list'''
 
 
 class MyList(list):
-    """Class MyList inherits from list."""
+    ''' Represents a MyList'''
 
     def print_sorted(self):
-        """Prints the list, in ascending sort."""
-
-        new_list = self[:]
-        new_list.sort()
-        print("{}".format(new_list))
+        '''prints the list, but sorted '''
+        print(sorted(self))
