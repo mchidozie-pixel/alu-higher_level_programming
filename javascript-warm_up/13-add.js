eab5cdeff5d107c249fb837de247b4b62c50d703
mchidozie-pixel <m.chidozie@alustudent.com>
2026-01-30 11:41:21 +0000

    a function that returns the addition of 2 integers.

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,3 +1,17 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-console.log(add(3, 5));
+const argsCount = process.argv.length;
+const numbers = [];
+switch (argsCount) {
+  case 2:
+  case 3:
+    console.log(0);
+    break;
+
+  default:
+    for (let i = 2; i < argsCount; i++) {
+      numbers.push(process.argv[i]);
+    }
+    numbers.sort((a, b) => b - a);
+    console.log(numbers[1]);
+    break;
+}
